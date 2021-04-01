98fb8fb417269b7bd0e8bbc119899c3fef3283d9
Daniel Diaz Granizo <daniel.diazg@scmspain.com>
2021-04-01 15:05:29 +0200

    aded basic

---
+++ RequestExecuter/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using RequestExecuter.Services;
+
+namespace RequestExecuter
+{
+    class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            await RunAsync(host.Services, args);
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(config =>
+                {
+                    config.AddJsonFile("AppSettings.json", optional: false);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<RequestService>();
+                });
+
+
+        private static async Task RunAsync(IServiceProvider services, string[] args)
+        {
+            await services.GetService<RequestService>().ExecuteAsync();
+        }
+
+
+    }
+}
---
+++ RequestExecuter/ConsoleApp1/Services/RequestService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestExecuter.Services
+{
+    public class RequestService
+    {
+        private readonly IConfiguration configuration;
+
+        public RequestService(IConfiguration configuration)
+        {
+            this.configuration = configuration 
+                    ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+
+        public async Task ExecuteAsync()
+        {
+            var name = configuration["Name"];
+            var url = configuration["Endpoint"];
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri($"{url}");
+
+            HttpResponseMessage response = await client.GetAsync("?name={name}");
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Ok!");
+            }
+            else
+            {
+                Console.WriteLine("Error!: {0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+            client.Dispose();
+        }
+    }
+}
